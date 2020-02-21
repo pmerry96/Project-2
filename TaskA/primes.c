8463c66ed97771fe3e9b16935f0ebf73560bcb3f
pmerry96 <pmerry@clemson.edu>
2020-02-21 14:20:00 -0500

    Task A -     attempt to formulate the primeSieve function, using pipes, forks, and recursion.     Commented out isolated and unaccessed code for testing - will remove when certain     the utilites arent needed Task B - attempt to solve pipeline command

--- TaskA/primes.c
+++ TaskA/primes.c
@@ -5,7 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h> //maybe dont include this
+#include <sys/wait.h>
 
+void primeSieve(int, int);
 
 /*
 int multiplesof(int* start_At, int up_to, int mulsof)
@@ -128,7 +130,7 @@ void primeSieve(int up_to, int pid)
     int incoming = read(p[0], &n, sizeof(n));
     if(incoming != 0)
     {
-    	printf("read Error in Primeseive, coming from PID = %d\n", pid)
+    	printf("read Error in Primeseive, coming from PID = %d\n", pid);
     }
     if(isprime(n))
         printf("pid=%d prime %d\n", pid, n);
